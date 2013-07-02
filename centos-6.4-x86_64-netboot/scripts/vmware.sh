188573266ad1bb351dca630fbf82a77c2e60c8ab
Steven Merrill <steven.merrill@gmail.com>
2013-07-01 22:23:04 -0400

    Update to support the latest VMWare tools feature.

--- centos-6.4-x86_64-netboot/scripts/vmware.sh
+++ centos-6.4-x86_64-netboot/scripts/vmware.sh
@@ -7,13 +7,13 @@
 
 #wget http://192.168.0.185/linux.iso -P /tmp
 mkdir -p /mnt/vmware
-mount -o loop /tmp/linux.iso /mnt/vmware
+mount -o loop /home/vagrant/linux.iso /mnt/vmware
 
 cd /tmp
 tar xzf /mnt/vmware/VMwareTools-*.tar.gz
 
 umount /mnt/vmware
-rm -fr /tmp/linux.iso
+rm -fr /home/vagrant/linux.iso
 
 /tmp/vmware-tools-distrib/vmware-install.pl -d
 rm -fr /tmp/vmware-tools-distrib
